8bce9b3ae611f579ff42bf0001068218bcbae810
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2024-02-19 20:16:31 +0100

    Update bundle

--- dist/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { App } from "vue";
-export { createColumn, createHiddenColumn, createLinkColumn, createActionColumn, createTextColumn, createCheckColumn, createSwitchColumn, createSelectColumn, createEmailColumn, createTelColumn } from "./functions/table-functions";
-import "./../lkt-table.css";
-declare const LktTable: {
-    install: (app: App) => void;
-};
-export default LktTable;
